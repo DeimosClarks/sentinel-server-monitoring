6f0db16c3970c632d5f727745b9418786aada9c7
DeimosC <dregnitor@gmail.com>
2026-05-17 20:35:58 +0200

    Initial setup

---
+++ src/main/java/sentinel_server_monitoring/SentinelServerMonitoringApplication.java
@@ -0,0 +1,13 @@
+package sentinel_server_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelServerMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelServerMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sentinel_server_monitoring/SentinelServerMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package sentinel_server_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelServerMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
